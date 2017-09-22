32667b04200999deeabd06d058cf0a95458423a9
Andrew Stellman <astellman@stellman-greene.com>
2017-09-22 17:07:39 -0500

    Added test to serialize and check statements from a large trig file

--- build.sbt
+++ build.sbt
@@ -12,8 +12,14 @@ libraryDependencies ++= Seq(
    "io.netty" % "netty-all" % "4.1.15.Final",
    "org.eclipse.rdf4j" % "rdf4j-runtime" % "2.2.2",
 
+   // Log dependencies
+   "org.slf4j" % "slf4j-api" % "1.7.25",
+   "org.slf4j" % "slf4j-simple" % "1.7.25",
+   "org.log4s" %% "log4s" % "1.3.5",
+
    // Test dependencies
-   "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+   "org.scalatest" %% "scalatest" % "3.0.1" % "test",
+   "com.github.pathikrit" %% "better-files" % "3.1.0" % "test"
 )
 
 // sbt-eclipse settings
--- src/test/scala/nettybufrdftest/ByteBufStatementSpec.scala
+++ src/test/scala/nettybufrdftest/ByteBufStatementSpec.scala
@@ -1,6 +1,7 @@
 package nettybufrdftest
 
 import scala.collection.concurrent.TrieMap
+import scala.collection.JavaConversions._
 
 import org.eclipse.rdf4j.model.Value
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory
@@ -12,8 +13,24 @@ import nettybufrdf.ByteBufStatement
 import nettybufrdf.ByteBufStatementWriter
 import nettybufrdf.MockCodexProvider
 import nettybufrdf.ByteBufStatementIterator
+import org.eclipse.rdf4j.rio.RDFFormat
+import org.eclipse.rdf4j.rio.ParserConfig
+import org.eclipse.rdf4j.rio.Rio
+import org.eclipse.rdf4j.model.impl.LinkedHashModel
+import org.eclipse.rdf4j.rio.helpers.StatementCollector
+import org.eclipse.rdf4j.rio.helpers.ParseErrorCollector
+import org.eclipse.rdf4j.rio.helpers.BasicParserSettings
+
+import better.files._
+import java.io.FileInputStream
+import scala.collection.mutable.Queue
+import org.eclipse.rdf4j.model.Statement
+
+import org.log4s._
 
 class ByteBufStatementSpec extends FlatSpec with Matchers {
+
+  private[this] val logger = getLogger
 
   behavior of "RDF ByteBuf iterator"
 
@@ -64,7 +81,7 @@ class ByteBufStatementSpec extends FlatSpec with Matchers {
     statements(1).getPredicate should be(s2.getPredicate)
     statements(1).getObject should be(s2.getObject)
     statements(1).getContext should be(s2.getContext)
-    
+
     statements(2).getSubject should be(s3.getSubject)
     statements(2).getPredicate should be(s3.getPredicate)
     statements(2).getObject should be(s3.getObject)
@@ -75,5 +92,46 @@ class ByteBufStatementSpec extends FlatSpec with Matchers {
     statements(3).getObject should be(s4.getObject)
     statements(3).getContext should be(s4.getContext)
 
+  }
+
+  it should "read a large trig file into a ByteBuf and iterate through it" in {
+    val settingsNoVerifyLangTag = new ParserConfig()
+    //settingsNoVerifyLangTag.set(BasicParserSettings.VERIFY_LANGUAGE_TAGS, false)
+    val errors = new ParseErrorCollector()
+    val parser = Rio.createParser(RDFFormat.TRIG)
+    val statementCollector = new StatementCollector(new LinkedHashModel())
+    parser.setRDFHandler(statementCollector)
+
+    val file = "src/test/resources/wnba-2015-season.trig".toFile
+    logger.info(s"Parsing RDF from ${file.pathAsString}")
+    val in = new FileInputStream(file.pathAsString)
+    parser.parse(in, "http://www.stellman-greene.com/pbprdf")
+    logger.info("Done parsing RDF")
+
+    val buf = ByteBufAllocator.DEFAULT.buffer(1024 * 1024 * 10)
+    val w = new ByteBufStatementWriter(buf, MockCodexProvider)
+
+    val statementsFromTrig = statementCollector.getStatements.toList
+
+    var start = System.currentTimeMillis
+    var count = 0
+    statementsFromTrig.foreach(statement => {
+      w.writeStatement(statement)
+      count += 1
+    })
+    logger.info(s"Serizlied ${count} statements to Netty buffer in ${System.currentTimeMillis - start}ms")
+
+    val statementsFromBuffer = Queue[Statement]()
+    new ByteBufStatementIterator(buf, MockCodexProvider, w.bnodeIdLookup).toList.foreach(statementsFromBuffer.enqueue(_))
+
+    statementsFromTrig.size should be(statementsFromBuffer.size)
+
+    start = System.currentTimeMillis
+    count = 0
+    statementsFromTrig.foreach(statementFromTrig => {
+      val statementFromBuffer = statementsFromBuffer.dequeue
+      count += 1
+    })
+    logger.info(s"Deserialized ${count} statements to Netty buffer and compared them in ${System.currentTimeMillis - start}ms")
   }
 }
